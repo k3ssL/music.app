f460a1c8f72556df85a002340264bac7dfce77e7
k3ssL <viqwer3@yandex.ru>
2024-09-17 01:20:30 +0500

    feat: connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common"
 import { TrackModule } from "./track/track.module"
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
-    imports: [TrackModule],
     controllers: [],
     providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB,
+            entities: [],
+            synchronize: true,
+        }),
+        TrackModule,
+    ],
 })
 export class AppModule {}
